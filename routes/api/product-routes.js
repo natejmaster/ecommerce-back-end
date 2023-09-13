707c8507f4b90b9ffbf900583e5f22f522c2564d
natejmaster@gmail.com <natejmaster@gmail.com>
2023-09-12 21:47:45 -0500

    Finalized api routes folder including tag-routes.js and product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -34,16 +34,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', async (req, res) => {
-  try {
-    const productData = await Product.create(req.body);
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-  /* req.body should look like this...
+/* req.body should look like this...
     {
       product_name: "Basketball",
       price: 200.00,
